b86ee6e45dfe53fb2eb2efe0c7ee4fb740a21f45
Dinesh <dineshveguru123@gmail.com>
2022-06-16 00:46:29 +0530

    made quick sort

---
+++ quick.py
@@ -0,0 +1,36 @@
+from turtle import end_fill
+from pandas import pivot
+
+
+n = int(input("enter number of elements: "))
+arr = []
+for i in range(n):
+    arr.append(int(input(f"enter {i + 1}th element: ")))
+
+
+def swap(arr, a, b):
+    temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp
+
+
+def partition(arr, start, end):
+    b = start - 1
+    pivot = arr[end]
+    for i in range(start, end + 1):
+        if arr[i] <= pivot:
+            b += 1
+            swap(arr, i, b)
+    return b
+
+
+def sort(arr, start, end):
+    if start >= end:
+        return
+    boundary = partition(arr, start, end)
+    sort(arr, start, boundary - 1)
+    sort(arr, boundary + 1, end)
+
+
+sort(arr, 0, len(arr) - 1)
+print(arr)
